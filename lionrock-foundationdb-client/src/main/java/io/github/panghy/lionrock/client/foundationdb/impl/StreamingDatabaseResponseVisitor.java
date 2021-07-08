f8005685cc0cd4cf1999fc7ef5dc9cae306d5106
Clement Pang <clement@wavefront.com>
2021-07-08 16:53:33 -0700

    StreamingDatabaseResponseVisitor for handling streaming responses.

---
+++ lionrock-foundationdb-client/src/main/java/io/github/panghy/lionrock/client/foundationdb/impl/StreamingDatabaseResponseVisitor.java
@@ -0,0 +1,35 @@
+package io.github.panghy.lionrock.client.foundationdb.impl;
+
+import io.github.panghy.lionrock.proto.*;
+
+/**
+ * Visitor interface for when a {@link io.github.panghy.lionrock.proto.StreamingDatabaseResponse}.
+ *
+ * @author Clement Pang
+ */
+public interface StreamingDatabaseResponseVisitor {
+
+  default void handleCommitTransaction(CommitTransactionResponse resp) {
+  }
+
+  default void handleGetValue(GetValueResponse resp) {
+  }
+
+  default void handleGetRange(GetRangeResponse resp) {
+  }
+
+  default void handleOperationFailure(OperationFailureResponse resp) {
+  }
+
+  default void handleGetVersionstamp(GetVersionstampResponse resp) {
+  }
+
+  default void handleGetReadVersion(GetReadVersionResponse resp) {
+  }
+
+  default void handleGetWatchKey(WatchKeyResponse resp) {
+  }
+
+  default void handleGetApproximateSize(GetApproximateSizeResponse resp) {
+  }
+}
