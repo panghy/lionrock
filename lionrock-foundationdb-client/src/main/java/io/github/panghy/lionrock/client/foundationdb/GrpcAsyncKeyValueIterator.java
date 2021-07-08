5e2e4f6ad71bb73249ddf82ee40069f9bb87f9e9
Clement Pang <clement@wavefront.com>
2021-07-07 17:04:15 -0700

    Commit AsyncIterator for foundationdb-grpc-client.

---
+++ lionrock-foundationdb-client/src/main/java/io/github/panghy/lionrock/client/foundationdb/GrpcAsyncKeyValueIterator.java
@@ -0,0 +1,142 @@
+package io.github.panghy.lionrock.client.foundationdb;
+
+import com.apple.foundationdb.FDBException;
+import com.apple.foundationdb.KeyValue;
+import com.apple.foundationdb.async.AsyncIterator;
+import com.apple.foundationdb.async.AsyncUtil;
+import io.github.panghy.lionrock.proto.GetRangeResponse;
+import io.github.panghy.lionrock.proto.OperationFailureResponse;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * An {@link AsyncIterator} backed by a gRPC call to a transaction (with streaming results coming back via the object
+ * {@link io.github.panghy.lionrock.proto.GetRangeResponse}.
+ *
+ * @author Clement Pang
+ */
+public class GrpcAsyncKeyValueIterator implements AsyncIterator<KeyValue> {
+
+  private final RemovalCallback removalCallback;
+  /**
+   * {@link LinkedList} to store {@link GetRangeResponse}s.
+   */
+  private final LinkedList<KeyValue> responses = new LinkedList<>();
+  /**
+   * {@link CompletableFuture} to signal when a result is available.
+   */
+  private CompletableFuture<Boolean> onHasNextFuture;
+  private boolean done = false;
+  private boolean cancelled = false;
+  private byte[] prevKey = null;
+
+  public GrpcAsyncKeyValueIterator(RemovalCallback removalCallback) {
+    this.removalCallback = removalCallback;
+  }
+
+  void accept(GetRangeResponse resp) {
+    if (onHasNextFuture.isCompletedExceptionally()) {
+      // eat the response if we have failed already.
+      return;
+    }
+    if (resp.getDone()) {
+      done = true;
+    }
+    synchronized (responses) {
+      boolean wasEmpty = responses.isEmpty();
+      // just adding more keys.
+      resp.getKeyValuesList().stream().
+          map(kv -> new KeyValue(kv.getKey().toByteArray(), kv.getValue().toByteArray())).
+          forEach(responses::add);
+      if (wasEmpty) {
+        // waiting for key values.
+        if (resp.getDone() && responses.isEmpty()) {
+          onHasNextFuture.complete(false);
+        } else {
+          onHasNextFuture.complete(true);
+        }
+      }
+    }
+  }
+
+  void accept(OperationFailureResponse failure) {
+    onHasNextFuture.completeExceptionally(new FDBException(failure.getMessage(), (int) failure.getCode()));
+  }
+
+  @Override
+  public CompletableFuture<Boolean> onHasNext() {
+    if (cancelled) {
+      throw new CancellationException();
+    }
+    if (!responses.isEmpty()) {
+      // has keys pending to be iterated.
+      return AsyncUtil.READY_TRUE;
+    } else if (onHasNextFuture != null && !onHasNextFuture.isDone()) {
+      // already has a future and it isn't done.
+      return onHasNextFuture;
+    } else if (done) {
+      // already done and no responses left.
+      return AsyncUtil.READY_FALSE;
+    }
+    synchronized (responses) {
+      if (responses.isEmpty() &&
+          (onHasNextFuture == null ||
+              (onHasNextFuture.isDone() &&
+                  !onHasNextFuture.isCompletedExceptionally()))) {
+        onHasNextFuture = new CompletableFuture<>();
+      }
+    }
+    return onHasNextFuture;
+  }
+
+  @Override
+  public boolean hasNext() {
+    if (cancelled) {
+      throw new CancellationException();
+    }
+    if (!responses.isEmpty()) {
+      return true;
+    } else if (done) {
+      return false;
+    }
+    return onHasNextFuture.join();
+  }
+
+  @Override
+  public KeyValue next() {
+    if (cancelled) {
+      throw new CancellationException();
+    }
+    if (!hasNext()) {
+      throw new NoSuchElementException();
+    }
+    synchronized (responses) {
+      KeyValue keyValue = responses.removeFirst();
+      prevKey = keyValue.getKey();
+      return keyValue;
+    }
+  }
+
+  @Override
+  public void cancel() {
+    cancelled = true;
+  }
+
+  @Override
+  public void remove() {
+    if (prevKey == null) {
+      throw new IllegalStateException("No value has been fetched from database");
+    }
+    removalCallback.deleteKey(prevKey);
+  }
+
+  /**
+   * Interface supplied to {@link GrpcAsyncKeyValueIterator} to delete a key during iteration.
+   */
+  public interface RemovalCallback {
+    void deleteKey(byte[] key);
+  }
+}
---
+++ lionrock-foundationdb-client/src/test/java/io/github/panghy/lionrock/client/foundationdb/GrpcAsyncKeyValueIteratorTest.java
@@ -0,0 +1,213 @@
+package io.github.panghy.lionrock.client.foundationdb;
+
+import com.apple.foundationdb.FDBException;
+import com.google.protobuf.ByteString;
+import io.github.panghy.lionrock.proto.GetRangeResponse;
+import io.github.panghy.lionrock.proto.KeyValue;
+import io.github.panghy.lionrock.proto.OperationFailureResponse;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class GrpcAsyncKeyValueIteratorTest {
+
+  @Test
+  public void testIterator() {
+    GrpcAsyncKeyValueIterator.RemovalCallback removalCallback = mock(GrpcAsyncKeyValueIterator.RemovalCallback.class);
+    GrpcAsyncKeyValueIterator iterator = new GrpcAsyncKeyValueIterator(removalCallback);
+
+    // add 1st and 2nd element and dequeue (also check remove()).
+    CompletableFuture<Boolean> cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+    iterator.accept(GetRangeResponse.newBuilder().
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world", StandardCharsets.UTF_8)).build()).
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello2", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world2", StandardCharsets.UTF_8)).build()).
+        build());
+    assertTrue(cf.isDone());
+    assertTrue(iterator.hasNext());
+    com.apple.foundationdb.KeyValue next = iterator.next();
+    assertArrayEquals("hello".getBytes(StandardCharsets.UTF_8), next.getKey());
+    cf = iterator.onHasNext();
+    assertTrue(cf.isDone());
+    assertTrue(iterator.hasNext());
+    next = iterator.next();
+    assertArrayEquals("hello2".getBytes(StandardCharsets.UTF_8), next.getKey());
+    iterator.remove();
+    verify(removalCallback, times(1)).deleteKey(eq("hello2".getBytes(StandardCharsets.UTF_8)));
+
+    // add 3rd and 4th element element as separate RPC calls before dequeueing.
+    cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+    cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+    iterator.accept(GetRangeResponse.newBuilder().
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello3", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world3", StandardCharsets.UTF_8)).build()).
+        build());
+    iterator.accept(GetRangeResponse.newBuilder().
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello4", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world4", StandardCharsets.UTF_8)).build()).
+        build());
+    assertTrue(cf.isDone());
+    assertTrue(iterator.hasNext());
+    next = iterator.next();
+    assertArrayEquals("hello3".getBytes(StandardCharsets.UTF_8), next.getKey());
+    cf = iterator.onHasNext();
+    assertTrue(cf.isDone());
+    assertTrue(iterator.hasNext());
+
+    // add 5th element before dequeueing.
+    iterator.accept(GetRangeResponse.newBuilder().
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello5", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world5", StandardCharsets.UTF_8)).build()).
+        build());
+    next = iterator.next();
+    assertArrayEquals("hello4".getBytes(StandardCharsets.UTF_8), next.getKey());
+    cf = iterator.onHasNext();
+    assertTrue(cf.isDone());
+    next = iterator.next();
+    assertArrayEquals("hello5".getBytes(StandardCharsets.UTF_8), next.getKey());
+
+    // add 6th element before dequeueing and mark done.
+    cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+    iterator.accept(GetRangeResponse.newBuilder().
+        setDone(true).
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello6", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world6", StandardCharsets.UTF_8)).build()).
+        build());
+    next = iterator.next();
+    assertArrayEquals("hello6".getBytes(StandardCharsets.UTF_8), next.getKey());
+    cf = iterator.onHasNext();
+    assertTrue(cf.isDone());
+    assertFalse(cf.join());
+    assertFalse(iterator.hasNext());
+  }
+
+  @Test
+  public void testIterator_failure() {
+    GrpcAsyncKeyValueIterator.RemovalCallback removalCallback = mock(GrpcAsyncKeyValueIterator.RemovalCallback.class);
+    GrpcAsyncKeyValueIterator iterator = new GrpcAsyncKeyValueIterator(removalCallback);
+
+    // add 1st and 2nd element.
+    CompletableFuture<Boolean> cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+    iterator.accept(GetRangeResponse.newBuilder().
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world", StandardCharsets.UTF_8)).build()).
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello2", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world2", StandardCharsets.UTF_8)).build()).
+        build());
+    assertTrue(cf.isDone());
+    assertTrue(iterator.hasNext());
+    com.apple.foundationdb.KeyValue next = iterator.next();
+    assertArrayEquals("hello".getBytes(StandardCharsets.UTF_8), next.getKey());
+    cf = iterator.onHasNext();
+    assertTrue(cf.isDone());
+    assertTrue(iterator.hasNext());
+    next = iterator.next();
+    assertArrayEquals("hello2".getBytes(StandardCharsets.UTF_8), next.getKey());
+
+    cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+
+    iterator.accept(OperationFailureResponse.newBuilder().setCode(123).setMessage("failed").build());
+    assertTrue(cf.isCompletedExceptionally());
+    FDBException join = (FDBException) cf.handle((aBoolean, throwable) -> throwable).join();
+    assertEquals(123, join.getCode());
+    assertEquals("failed", join.getMessage());
+
+    // does not change the state.
+    iterator.accept(GetRangeResponse.newBuilder().
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world", StandardCharsets.UTF_8)).build()).
+        addKeyValues(
+            KeyValue.newBuilder().
+                setKey(ByteString.copyFrom("hello2", StandardCharsets.UTF_8)).
+                setValue(ByteString.copyFrom("world2", StandardCharsets.UTF_8)).build()).
+        build());
+
+    cf = iterator.onHasNext();
+    assertTrue(cf.isCompletedExceptionally());
+  }
+
+  @Test
+  public void testIterator_emptyResponse() {
+    GrpcAsyncKeyValueIterator.RemovalCallback removalCallback = mock(GrpcAsyncKeyValueIterator.RemovalCallback.class);
+    GrpcAsyncKeyValueIterator iterator = new GrpcAsyncKeyValueIterator(removalCallback);
+
+    // add 1st and 2nd element and dequeue (also check remove()).
+    CompletableFuture<Boolean> cf = iterator.onHasNext();
+    assertFalse(cf.isDone());
+
+    iterator.accept(GetRangeResponse.newBuilder().
+        setDone(true).
+        build());
+    assertTrue(cf.isDone());
+    assertTrue(cf.isDone());
+    assertFalse(cf.join());
+
+    cf = iterator.onHasNext();
+    assertTrue(cf.isDone());
+    assertFalse(cf.join());
+    assertFalse(iterator.hasNext());
+    try {
+      iterator.next();
+      fail();
+    } catch (NoSuchElementException expected) {
+    }
+  }
+
+  @Test()
+  public void testIterator_cancel() {
+    GrpcAsyncKeyValueIterator.RemovalCallback removalCallback = mock(GrpcAsyncKeyValueIterator.RemovalCallback.class);
+    GrpcAsyncKeyValueIterator iterator = new GrpcAsyncKeyValueIterator(removalCallback);
+    try {
+      // can't remove before having first fetch.
+      iterator.remove();
+      fail();
+    } catch (IllegalStateException expected) {
+    }
+    iterator.cancel();
+    try {
+      iterator.onHasNext();
+      fail();
+    } catch (IllegalStateException expected) {
+    }
+    try {
+      iterator.hasNext();
+      fail();
+    } catch (IllegalStateException expected) {
+    }
+    try {
+      iterator.next();
+      fail();
+    } catch (IllegalStateException expected) {
+    }
+  }
+}
