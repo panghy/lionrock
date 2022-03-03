06e34613eab2c339fc2fa88d761318254a782ed4
Clement Pang <clement@wavefront.com>
2022-03-03 17:23:35 +0800

    Fix start.bash

--- lionrock-foundationdb-server/release/start.bash
+++ lionrock-foundationdb-server/release/start.bash
@@ -22,8 +22,26 @@
 set -xe;
 
 if [[ -z "${FDB_CLUSTER_FILE:-}" || ! -s "${FDB_CLUSTER_FILE}" ]]; then
-    /bash/create_cluster_file.bash
-    FDB_CLUSTER_FILE="${FDB_CLUSTER_FILE:-/etc/foundationdb/fdb.cluster}"
+
+    # from https://github.com/apple/foundationdb/blob/main/packaging/docker/fdb.bash
+    FDB_CLUSTER_FILE=${FDB_CLUSTER_FILE:-/etc/foundationdb/fdb.cluster}
+    mkdir -p "$(dirname $FDB_CLUSTER_FILE)"
+
+    if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
+        echo "$FDB_CLUSTER_FILE_CONTENTS" > "$FDB_CLUSTER_FILE"
+    elif [[ -n $FDB_COORDINATOR ]]; then
+        coordinator_ip=$(dig +short "$FDB_COORDINATOR")
+        if [[ -z "$coordinator_ip" ]]; then
+            echo "Failed to look up coordinator address for $FDB_COORDINATOR" 1>&2
+            exit 1
+        fi
+        coordinator_port=${FDB_COORDINATOR_PORT:-4500}
+        echo "docker:docker@$coordinator_ip:$coordinator_port" > "$FDB_CLUSTER_FILE"
+    else
+        echo "FDB_COORDINATOR environment variable not defined" 1>&2
+        exit 1
+    fi
+    # end from https://github.com/apple/foundationdb/blob/main/packaging/docker/fdb.bash
 
     # Attempt to connect. Configure the database if necessary.
     if ! /usr/bin/fdbcli -C "${FDB_CLUSTER_FILE}" --exec status --timeout 3 ; then
